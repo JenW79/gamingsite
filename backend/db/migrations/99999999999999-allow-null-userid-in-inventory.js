7354c3accbd8fa0cc14838080622665c8236c5eb
JenW79 <jkwill646@gmail.com>
2025-06-02 18:34:02 -0500

    Ensure allow-null migration runs last

--- backend/db/migrations/99999999999999-allow-null-userid-in-inventory.js
+++ backend/db/migrations/99999999999999-allow-null-userid-in-inventory.js
@@ -1,33 +1,36 @@
-'use strict';
+"use strict";
 
 let options = {};
 if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA;
 }
-options.tableName = "Inventories"; 
+options.tableName = "Inventories";
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.changeColumn(options, 'userId', {
+    await queryInterface.removeColumn(options, "userId");
+    await queryInterface.addColumn(options.tableName, "userId", {
       type: Sequelize.INTEGER,
       allowNull: true,
       references: {
-        model: 'Users',
-        key: 'id',
+        model: "Users",
+        key: "id",
       },
-      onDelete: 'CASCADE',
+      onDelete: "CASCADE",
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.changeColumn(options, 'userId', {
+    await queryInterface.removeColumn(options, "userId");
+
+    await queryInterface.addColumn(options.tableName, "userId", {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {
-        model: 'Users',
-        key: 'id',
+        model: "Users",
+        key: "id",
       },
-      onDelete: 'CASCADE',
+      onDelete: "CASCADE",
     });
   },
 };
