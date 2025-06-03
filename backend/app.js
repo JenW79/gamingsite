d2713bc917a31f7026d0e30959891aad866cef05
JenW79 <jkwill646@gmail.com>
2025-06-03 10:09:35 -0500

    configure helmet

--- backend/app.js
+++ backend/app.js
@@ -36,15 +36,32 @@ app.use(
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
+        scriptSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "https://cdnjs.cloudflare.com",
+        ],
+        styleSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "https://fonts.googleapis.com",
+          "https://cdnjs.cloudflare.com",
+        ],
+        styleSrcElem: [
+          "'self'",
+          "https://fonts.googleapis.com",
+          "https://cdnjs.cloudflare.com",
+        ],
+        fontSrc: ["'self'", "https://fonts.gstatic.com"],
         imgSrc: ["'self'", "data:", "https:"],
         connectSrc: [
           "'self'",
           allowedFrontend,
-          allowedFrontend.replace(/^http/, "ws"), // supports ws:// or wss://
+          allowedFrontend.replace(/^http/, "ws"),
           "https:",
+          "http:",
           "wss:",
+          "ws:",
         ],
       },
     },
