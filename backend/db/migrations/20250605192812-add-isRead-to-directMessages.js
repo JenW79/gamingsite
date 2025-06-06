926ac624109b9a2062a05f29e4d77724ff772a86
JenW79 <jkwill646@gmail.com>
2025-06-05 20:20:22 -0500

    adding migration fix

--- backend/db/migrations/20250605192812-add-isRead-to-directMessages.js
+++ backend/db/migrations/20250605192812-add-isRead-to-directMessages.js
@@ -1,9 +1,8 @@
 "use strict";
 let options = {};
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production" && process.env.SCHEMA) {
   options.schema = process.env.SCHEMA;
 }
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
