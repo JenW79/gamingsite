8bb45c6f5ea984d98bfe8e600aa8798df82ac178
JenW79 <jkwill646@gmail.com>
2025-06-29 16:34:27 -0500

    updated api/socket

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
 // backend/server.js
 const http = require("http");
-const app = require("./app"); 
+const app = require("./app");
 const setupSockets = require("./socket");
 
 const server = http.createServer(app);
 
-// setupSockets(server); 
 
-const io = setupSockets(server);
-app.set("io", io);
-
-
+const io = setupSockets(server); 
+app.set("io", io);               
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
